dd334bb23755985fa0284921515e43675c0a3dde
yuanx <i@jerryzone.cn>
2021-06-03 20:39:22 +0800

    完成W25X读写

--- src/main.c
+++ src/main.c
@@ -1,14 +1,18 @@
 #include "stm32f10x_conf.h"
 #include <usart.h>
 #include <oled.h>
+#include <w25x.h>
 #include <SGUI_Text.h>
 #include <SGUI_FontResource.h>
 
 int main(void) {
-	SGUI_SCR_DEV screen;
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
+	char pstTips[20]={0};
+    SGUI_SCR_DEV screen;
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
     USART_Config();
     OLED_Initialize();
+    W25X_Initialize();
+
     screen.stSize.iWidth=128;
     screen.stSize.iHeight=64;
     screen.fnClear=OLED_Clear;
@@ -16,12 +20,22 @@ int main(void) {
     screen.fnSetPixel=OLED_SetPixel;
     screen.fnSyncBuffer=OLED_Commit;
 
-    SGUI_RECT rect={0,0,128,12};
-    SGUI_POINT point={0,0};
+    SGUI_RECT rect= {0,0,128,12};
+    SGUI_POINT point= {0,0};
 
     SGUI_Text_DrawText(&screen,"System Work Well!",SGUI_FONT_REF(FONT_12),&rect,&point,1);
+    OLED_Commit();
+
+    W25XJEDECId_t j;
+    W25X_Read_JEDECId(&j);
+    sprintf(pstTips,"id=0x%04X,manu=0x%04X",j.uiFlashId,j.uiManufacture);
+    rect.iY += 12;
+	SGUI_Text_DrawText(&screen,pstTips,SGUI_FONT_REF(FONT_12),&rect,&point,1);
 	OLED_Commit();
     while(1) {
-
     }
+}
+void HardFault_Handler() {
+
+    while(1);
 }
---
+++ src/w25x.c
@@ -0,0 +1,350 @@
+#include <w25x.h>
+#include <stm32f10x.h>
+#include <stdio.h>
+
+#define W25X_DMA_Channel_Rx DMA1_Channel4
+#define W25X_DMA_Channel_Tx DMA1_Channel5
+
+static uint8_t SectorBuffer[W25X_SECTOR_SIZE]={0};
+
+void W25X_Wait_Busy();
+
+void W25X_Initialize() {
+
+    // 使能GPIO引脚时钟
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB|RCC_APB2Periph_AFIO, ENABLE);
+
+    /**
+     * 初始化GPIO
+     */
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+    GPIO_InitStructure.GPIO_Pin =GPIO_Pin_12; //PA15 =1
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
+    // 临时禁用Flash
+    GPIO_SetBits(GPIOB,GPIO_Pin_12);
+
+    /**
+     * 初始化SPI3
+     */
+
+    // 使能SPI3时钟
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_SPI2, ENABLE);
+    SPI_InitTypeDef SPI_InitStructure;
+
+    SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
+    SPI_InitStructure.SPI_Mode = SPI_Mode_Master;
+    SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;
+    SPI_InitStructure.SPI_CPOL = SPI_CPOL_High;
+    SPI_InitStructure.SPI_CPHA = SPI_CPHA_2Edge;
+    SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;
+    SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_4;
+    SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
+    SPI_InitStructure.SPI_CRCPolynomial = 7;
+
+    // 初始化SPI3
+    SPI_Init(SPI2, &SPI_InitStructure);
+    // 使能SPI3
+    SPI_Cmd(SPI2, ENABLE);
+
+    /**
+     * 初始化DMA2
+     * DMA2->Channel1 ---- 接收
+     * DMA2->Channel2 ---- 发送
+     */
+
+    // 启用DMA2时钟
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+
+    DMA_InitTypeDef DMA_InitStructure;
+    // 初始化公用DMA结构体
+    DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&SPI2->DR;
+    DMA_InitStructure.DMA_MemoryBaseAddr     = (uint32_t)0;
+    DMA_InitStructure.DMA_BufferSize		 = (uint32_t)0;
+    DMA_InitStructure.DMA_PeripheralInc      = DMA_PeripheralInc_Disable;
+    DMA_InitStructure.DMA_MemoryInc          = DMA_MemoryInc_Enable;
+    DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
+    DMA_InitStructure.DMA_MemoryDataSize     = DMA_MemoryDataSize_Byte;
+    DMA_InitStructure.DMA_Mode               = DMA_Mode_Normal;
+    DMA_InitStructure.DMA_Priority           = DMA_Priority_VeryHigh;
+    DMA_InitStructure.DMA_M2M                = DMA_M2M_Disable;
+
+    // 初始化DMA接收通道
+    DMA_DeInit(W25X_DMA_Channel_Rx);
+    DMA_InitStructure.DMA_DIR                = DMA_DIR_PeripheralSRC;
+    DMA_Init(W25X_DMA_Channel_Rx, &DMA_InitStructure);
+
+	// 初始化DMA发送通道
+	DMA_DeInit(W25X_DMA_Channel_Tx);
+	DMA_InitStructure.DMA_DIR                = DMA_DIR_PeripheralDST;
+    DMA_Init(W25X_DMA_Channel_Tx, &DMA_InitStructure);
+
+	// 使能SPI3通过DMA收发数据
+    SPI_I2S_DMACmd(SPI2, SPI_I2S_DMAReq_Rx, ENABLE);
+    SPI_I2S_DMACmd(SPI2, SPI_I2S_DMAReq_Tx, ENABLE);
+
+}
+
+void W25X_ReceiveBuffer(void* buffer,uint16_t length){
+	uint8_t dummy = 0xFF;
+	// 清除中断
+	DMA_ClearITPendingBit(DMA1_IT_GL4|DMA1_IT_GL5);
+	DMA_ClearFlag(DMA1_FLAG_GL4|DMA1_FLAG_GL5);
+
+	dummy=SPI2->DR;
+	dummy=0xFF;
+
+	// SPI接收通道(DMA2_Channel1)
+	W25X_DMA_Channel_Rx->CNDTR  = (uint16_t)length;				// 拷贝数量			length
+	W25X_DMA_Channel_Rx->CCR   |= DMA_MemoryInc_Enable;			// 内存增长模式		增长
+	W25X_DMA_Channel_Rx->CMAR   = (uint32_t)buffer;				// 内存地址			buffer
+
+	// SPI发送通道(DMA2_Channel2)
+	W25X_DMA_Channel_Tx->CNDTR  = (uint16_t)length;				// 拷贝数量			length
+	W25X_DMA_Channel_Tx->CCR   &= ~DMA_MemoryInc_Enable;		// 内存增长模式		不增长
+	W25X_DMA_Channel_Tx->CMAR   = (uint32_t)&dummy;				// 内存地址			buffer
+
+	// 启动DMA
+	DMA_Cmd(W25X_DMA_Channel_Rx,ENABLE);
+	DMA_Cmd(W25X_DMA_Channel_Tx,ENABLE);
+
+	while(DMA_GetFlagStatus(DMA1_FLAG_TC4)==RESET);	//TODO: 改成信号量调度的
+	// 关掉DMA
+	DMA_Cmd(W25X_DMA_Channel_Rx,DISABLE);
+	DMA_Cmd(W25X_DMA_Channel_Tx,DISABLE);
+}
+void W25X_SendBuffer(const void* buffer,uint16_t length) {
+    uint8_t dummy = 0xFF;
+	// 清除中断
+	DMA_ClearITPendingBit(DMA1_IT_GL4|DMA1_IT_GL5);
+	DMA_ClearFlag(DMA1_FLAG_GL4|DMA1_FLAG_GL5);
+
+	// 清除SPI中残留的数据
+	SPI2->DR;
+
+	// SPI接收通道(DMA1_Channel4)
+	W25X_DMA_Channel_Rx->CNDTR  = (uint16_t)length;			// 拷贝数量			length
+	W25X_DMA_Channel_Rx->CCR   &= ~DMA_MemoryInc_Enable;	// 内存增长模式		不增长
+	W25X_DMA_Channel_Rx->CMAR   = (uint32_t)&dummy;			// 内存地址			dummy
+
+	// SPI发送通道(DMA1_Channel5)
+	W25X_DMA_Channel_Tx->CNDTR  = (uint16_t)length;			// 拷贝数量			length
+	W25X_DMA_Channel_Tx->CCR   |= DMA_MemoryInc_Enable;		// 内存增长模式		增长
+	W25X_DMA_Channel_Tx->CMAR   = (uint32_t)buffer;			// 内存地址			buffer
+
+	// 启动DMA
+	DMA_Cmd(W25X_DMA_Channel_Rx,ENABLE);
+	DMA_Cmd(W25X_DMA_Channel_Tx,ENABLE);
+
+	while(DMA_GetFlagStatus(DMA1_FLAG_TC5)==RESET){
+
+	}
+	// 关掉DMA
+	DMA_Cmd(W25X_DMA_Channel_Rx,DISABLE);
+	DMA_Cmd(W25X_DMA_Channel_Tx,DISABLE);
+}
+
+void W25X_Set_WriteState(FunctionalState writeState) {
+    const uint8_t enableCmd[] = {W25X_CMD_WRITE_ENABLE};
+    const uint8_t disableCmd[]= {W25X_CMD_WRITE_DISABLE};
+    GPIO_ResetBits(GPIOB,GPIO_Pin_12);
+    if(writeState==ENABLE) {
+        W25X_SendBuffer(enableCmd,sizeof(enableCmd));
+    } else {
+        W25X_SendBuffer(disableCmd,sizeof(disableCmd));
+    }
+    GPIO_SetBits(GPIOB,GPIO_Pin_12);
+
+}
+void W25X_Read_Status(W25XStatus_t* puiStatus) {
+    const uint8_t command[]= {W25X_CMD_READ_STATUS};
+
+    GPIO_ResetBits(GPIOB,GPIO_Pin_12);
+    W25X_SendBuffer(command,sizeof(command));
+    W25X_ReceiveBuffer(puiStatus,sizeof(W25XStatus_t));
+    GPIO_SetBits(GPIOB,GPIO_Pin_12);
+}
+void W25X_Write_Status(W25XStatus_t puiStatus) {
+    const uint8_t command[]= {W25X_CMD_WRITE_STATUS};
+
+    GPIO_ResetBits(GPIOB,GPIO_Pin_12);
+    W25X_SendBuffer(command,sizeof(command));
+    W25X_SendBuffer(&puiStatus,sizeof(W25XStatus_t));
+    GPIO_SetBits(GPIOB,GPIO_Pin_12);
+}
+void W25X_Read_Data(uint32_t uiAddress,void* pBuffer,uint32_t uiLength) {
+    const uint8_t command[]= {W25X_CMD_READ_DATA};
+    uint8_t addr[3];
+    addr[0] = ((uiAddress & 0xFF0000)>>16)&0xFF;
+    addr[1] = ((uiAddress & 0x00FF00)>>8) &0xFF;
+    addr[2] = ((uiAddress & 0x0000FF)>>0) &0xFF;
+    GPIO_ResetBits(GPIOB,GPIO_Pin_12);
+    W25X_SendBuffer(command,sizeof(command));
+    W25X_SendBuffer(addr,sizeof(addr));
+    W25X_ReceiveBuffer(pBuffer,uiLength);
+    GPIO_SetBits(GPIOB,GPIO_Pin_12);
+}
+void W25X_Read_Data_Fast(uint32_t uiAddress,void* pBuffer,uint32_t uiLength) {
+    const uint8_t command[]= {W25X_CMD_FAST_READ};
+    uint8_t addr[4];
+    addr[0] = ((uiAddress & 0xFF0000)>>16)&0xFF;
+    addr[1] = ((uiAddress & 0x00FF00)>>8) &0xFF;
+    addr[2] = ((uiAddress & 0x0000FF)>>0) &0xFF;
+    addr[3] = W25X_DUMMY_BYTE;
+    GPIO_ResetBits(GPIOB,GPIO_Pin_12);
+    W25X_SendBuffer(command,sizeof(command));
+    W25X_SendBuffer(addr,sizeof(addr));
+    W25X_ReceiveBuffer(pBuffer,uiLength);
+    GPIO_SetBits(GPIOB,GPIO_Pin_12);
+}
+void W25X_Write_Page(uint32_t uiAddress,void* pBuffer,uint32_t uiLength) {
+    const uint8_t command[]= {W25X_CMD_PAGE_PROGRAM};
+    uint8_t addr[3];
+    uint16_t byteToWrite = uiLength<W25X_MAX_PAGE?uiLength:W25X_MAX_PAGE;
+
+    W25X_Set_WriteState(ENABLE);
+
+    GPIO_ResetBits(GPIOB,GPIO_Pin_12);
+    addr[0] = ((uiAddress & 0xFF0000)>>16)&0xFF;
+    addr[1] = ((uiAddress & 0x00FF00)>>8) &0xFF;
+    addr[2] = ((uiAddress & 0x0000FF)>>0) &0xFF;
+    W25X_SendBuffer(command,sizeof(command));
+    W25X_SendBuffer(addr,sizeof(addr));
+    W25X_SendBuffer(pBuffer,byteToWrite);
+    GPIO_SetBits(GPIOB,GPIO_Pin_12);
+}
+void W25X_Erase_Block(uint32_t uiAddress) {
+    const uint8_t command[]= {W25X_CMD_ERASE_BLOCK};
+    uint8_t addr[3];
+
+    uiAddress -= uiAddress % 0x10000;			// 对齐64K
+
+    addr[0] = ((uiAddress & 0xFF0000)>>16)&0xFF;
+    addr[1] = ((uiAddress & 0x00FF00)>>8) &0xFF;
+    addr[2] = ((uiAddress & 0x0000FF)>>0) &0xFF;
+
+    W25X_Set_WriteState(ENABLE);
+
+    GPIO_ResetBits(GPIOB,GPIO_Pin_12);
+    W25X_SendBuffer(command,sizeof(command));
+    W25X_SendBuffer(addr,sizeof(addr));
+    GPIO_SetBits(GPIOB,GPIO_Pin_12);
+
+}
+void W25X_Erase_Sector(uint32_t uiAddress) {
+    const uint8_t command[]= {W25X_CMD_ERASE_SECTOR};
+    uint8_t addr[3];
+
+    uiAddress -= uiAddress % 0x01000;			// 对齐4K
+
+    addr[0] = ((uiAddress & 0xFF0000)>>16)&0xFF;
+    addr[1] = ((uiAddress & 0x00FF00)>>8) &0xFF;
+    addr[2] = ((uiAddress & 0x0000FF)>>0) &0xFF;
+
+    W25X_Set_WriteState(ENABLE);
+
+    GPIO_ResetBits(GPIOB,GPIO_Pin_12);
+    W25X_SendBuffer(command,sizeof(command));
+    W25X_SendBuffer(addr,sizeof(addr));
+    GPIO_SetBits(GPIOB,GPIO_Pin_12);
+
+
+    W25X_Wait_Busy();
+}
+void W25X_Erase_Chip() {
+    const uint8_t command[]= {W25X_CMD_ERASE_CHIP};
+
+    W25X_Set_WriteState(ENABLE);
+
+    GPIO_ResetBits(GPIOB,GPIO_Pin_12);
+    W25X_SendBuffer(command,sizeof(command));
+    GPIO_SetBits(GPIOB,GPIO_Pin_12);
+}
+void W25X_Read_JEDECId(W25XJEDECId_t* pstJEDECId) {
+    const uint8_t command[]= {W25X_CMD_READ_JEDEC_ID};
+
+    GPIO_ResetBits(GPIOB,GPIO_Pin_12);
+    W25X_SendBuffer(command,sizeof(command));
+    W25X_ReceiveBuffer(pstJEDECId,sizeof(W25XJEDECId_t));
+    GPIO_SetBits(GPIOB,GPIO_Pin_12);
+
+    pstJEDECId->uiFlashId = ((pstJEDECId->uiFlashId>>8)&0x00FF)|((pstJEDECId->uiFlashId<<8)&0xFF00);
+}
+void W25X_Wait_Busy() {
+    W25XStatus_t uiStatus;
+    W25X_Read_Status(&uiStatus);
+    while(uiStatus & W25X_STATUS_MASK_BUSY) {
+        W25X_Read_Status(&uiStatus);
+    }
+}
+#define MIN(A,B) ((A)<(B)?(A):(B))
+void W25X_Write_Buffer(uint32_t uiAddress,uint8_t* pBuffer,uint32_t uiLength) {
+	uint32_t uiSectorAddress;		// 扇区的首地址
+	uint32_t uiSectorWriteAddress;	// (每扇区)写入位置的地址
+	uint8_t* puiSectorBuffer;		// (每扇区)写入数据来源
+	uint16_t uiAddressOffset;		// (每扇区)写入数据的位置相对扇区首地址的偏移量
+	uint16_t uiWriteLength;			// (每扇区)写入的pBuffer长度
+    uint16_t uiSectorWriteLength;	// (每扇区)实际需要写入的长度
+    uint16_t  uiPageWriteLength;		// (每页)需要写入的长度
+    uint8_t	 uiFlags;				// 擦除必要性标志
+
+    // 计算涉及写入的第一个扇区的首地址
+    uiSectorAddress = uiAddress - uiAddress % W25X_SECTOR_SIZE;
+    // 每次循环处理一个扇区写入
+	while(uiLength>0){
+		W25X_Wait_Busy();
+		uiFlags = 0;
+		// 计算写入位置相对当前扇区起始位置的偏移
+		uiAddressOffset = uiAddress-uiSectorAddress;
+		// 当前扇区要写入的数据量 = 剩余要写的量 和 扇区剩余可写的量 中较小的一个
+		uiWriteLength = MIN(W25X_SECTOR_SIZE-uiAddressOffset,uiLength);
+		// 读出当前扇区现有的数据
+		W25X_Read_Data(uiSectorAddress,SectorBuffer,W25X_SECTOR_SIZE);
+		//dumpMemory(SectorBuffer,W25X_SECTOR_SIZE);
+		// 检查是否需要擦除
+		for(uint16_t uiByteIndex=0;uiByteIndex<uiWriteLength;uiByteIndex++){
+			// ~原有 & 新的 = 是否有位从0变1了 ==> 需要擦除
+			uiFlags |= ((~SectorBuffer[uiAddressOffset+uiByteIndex])&pBuffer[uiByteIndex])?1:0;
+			// 将数据覆盖到缓冲区
+			SectorBuffer[uiAddressOffset+uiByteIndex] = pBuffer[uiByteIndex];
+		}
+		if(uiFlags){
+			// 需要擦除 => 擦除扇区,修改扇区缓冲中的数据,然后完整写回
+			// 擦除扇区
+			W25X_Erase_Sector(uiSectorAddress);
+			// 修改写入位置的信息(从头写,写一个扇区)
+			uiSectorWriteAddress = uiSectorAddress;
+			uiSectorWriteLength = W25X_SECTOR_SIZE;
+			puiSectorBuffer     = SectorBuffer;
+		}else{
+			// 无需擦除 => 直接写入对应区域即可
+			uiSectorWriteAddress = uiAddress;
+			uiSectorWriteLength = uiWriteLength;
+			puiSectorBuffer     = SectorBuffer+uiAddressOffset;
+		}
+		// 开始页编程
+		while(uiSectorWriteLength>0){
+			// 计算当前页最多能写入的长度
+			uiPageWriteLength = MIN(W25X_MAX_PAGE,uiSectorWriteLength);
+			// 写入一页
+			W25X_Write_Page(uiSectorWriteAddress,puiSectorBuffer,uiPageWriteLength);
+			W25X_Wait_Busy();
+			// 更新写入地址与剩余长度
+			uiSectorWriteAddress += uiPageWriteLength;
+			puiSectorBuffer      += uiPageWriteLength;
+			uiSectorWriteLength  -= uiPageWriteLength;
+		}
+		// 更新地址
+		uiSectorAddress += W25X_SECTOR_SIZE;
+		uiAddress 		+= uiWriteLength;
+		pBuffer 		+= uiWriteLength;
+		uiLength        -= uiWriteLength;
+	}
+	return;
+}
